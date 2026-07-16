220248523c8892e007e6a4c28010c657a8f400b9
abasasdzadh <system88my@gmail.com>
2026-07-16 22:45:51 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', // مسیرها را نسبی می‌کند تا استایل‌ها روی وب‌ویو گوشی لود شوند
   server: {
     port: 3000,
     open: true
   },
   build: {
-    outDir: 'www', // خروجی نهایی به پوشه پیش‌فرض خازنی خروجی می‌دهد
+    outDir: 'www',
     emptyOutDir: true
   }
 });
